72dc6c5ddbee334677530a863608f0111cb495c4
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-05-15 17:32:08 +0200

    Docstring addition

--- algorithms.py
+++ algorithms.py
@@ -3,14 +3,30 @@ import numpy as np
 import scipy as sp
 
 
-def page_rank(G: nx.digraph, iterations=100, alpha=0.85, error=1.0e-6):
+def page_rank(G: nx.digraph,
+              iterations=100,
+              alpha=0.85,
+              error=1.0e-6) -> dict:
+    """This function implements the PageRank algorithm.
 
+    Args:
+        G (nx.digraph): a networkx digraph
+        iterations (int, optional): number of maximum iterations. Defaults to 100.
+        alpha (float, optional): damping factor. Defaults to 0.85.
+        error (float, optional): error threshold to reach to break the iterations. Defaults to 1.0e-6.
+
+    Returns:
+        dict: a dictionary of nodes and their PageRank values
+    """
     if len(G) == 0:
         return {}
 
     nodes = list(G)
 
-    A = nx.to_numpy_array(G, nodelist=nodes, weight="weight", dtype=float)
+    A = nx.to_numpy_array(G,
+                          nodelist=nodes,
+                          weight="weight",
+                          dtype=float)
     n, _ = A.shape
 
     if n == 0:
@@ -33,4 +49,5 @@ def page_rank(G: nx.digraph, iterations=100, alpha=0.85, error=1.0e-6):
         err = np.absolute(x - y).sum()
         if err < n * error:
             return dict(zip(nodes, map(float, x)))
+
     return dict(zip(nodes, map(float, x)))
