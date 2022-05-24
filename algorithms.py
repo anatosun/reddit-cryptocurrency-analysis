ddf45ee28a28acaa70bb095c0a22ceaf224eff48
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-05-24 11:58:21 +0200

    Main removal

--- algorithms.py
+++ algorithms.py
@@ -1,7 +1,5 @@
 import networkx as nx
 import numpy as np
-import timeit
-import resource
 
 
 def page_rank(G: nx.digraph,
@@ -53,37 +51,3 @@ def page_rank(G: nx.digraph,
             return dict(zip(nodes, map(float, x)))
 
     return dict(zip(nodes, map(float, x)))
-
-
-def test_page_rank():
-    import pandas as pd
-    G = nx.from_pandas_edgelist(pd.read_csv(
-        './data/edges_deep_link_no_merge.csv'), create_using=nx.DiGraph())
-    start = timeit.default_timer()
-    nx_ranks = nx.pagerank(G)
-    stop = timeit.default_timer()
-    nx_time = stop - start
-    print('networkx pagerank computation time: {}s'.format(nx_time))
-    nx_memory = resource.getrusage(
-        resource.RUSAGE_SELF).ru_maxrss/1024.0/1024.0
-    print('networkx pagerank memory usage: {}MB'.format(nx_memory))
-    start = timeit.default_timer()
-    own_ranks = page_rank(G)
-    stop = timeit.default_timer()
-    own_time = stop - start
-    own_memory = resource.getrusage(
-        resource.RUSAGE_SELF).ru_maxrss/1024.0/1024.0
-    print('own pagerank implementation computation time: {}s'.format(own_time))
-    print('own pagerank implementation memory usage: {}MB'.format(own_memory))
-    overall_error = np.array(
-        [np.absolute(own_ranks[k]-nx_ranks[k]) for k in own_ranks.keys()]).sum()
-    print(
-        f"overall_error from own implementation to networkx's one: {overall_error}")
-
-
-def main():
-    test_page_rank()
-
-
-if __name__ == "__main__":
-    main()
